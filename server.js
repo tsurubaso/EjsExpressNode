22b23e331426178e612ce9a644a2b446cadc13ce
LAPTOP-L82MDLB4\user <tsurubaso@gmail.com>
2023-05-02 00:33:23 +0900

    20.34 ajout de ejs

--- server.js
+++ server.js
@@ -2,17 +2,16 @@ const express = require("express");
 const app = express();
 const userRouter=require("./routes/user")
 
-
-
 const PORT = 3000;
-
+app.use(express.static("public"));
 app.get("/", (req, res) => {
   //console.log("tell me all of the essentials")
-  //res.send("best known than forgotten")
+  res.send("best known than forgotten")
   //res.sendStatus(404);
 });
 
 app.use("/user", userRouter);
+app.set("view engine", "ejs")
 
 
 
